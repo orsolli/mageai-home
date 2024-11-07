89af6049a564cb68877745069bc14218432cb691
orsolli <orjansolli@outlook.com>
2024-11-07 21:51:18 +0000

    Create pipeline Alert failures

---
+++ data_loaders/run_status.py
@@ -0,0 +1,30 @@
+from mage_ai.orchestration.run_status_checker import check_status
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+import pandas as pd
+
+@data_loader
+def load_data(*args, **kwargs):
+    """
+    Template code for loading data from any source.
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    if not check_status(
+        'entso_e',
+        kwargs['execution_date'],
+        block_uuid='store_energy_price_file',
+        hours=24,
+    ):
+        return 'Failed to run Entso-E pipeline'
+
+    if not check_status(
+        'alert_energy_price',
+        kwargs['execution_date'],
+        block_uuid='email_sender',
+        hours=24,
+    ):
+        return 'Failed to run Alert Energy Price pipeline'
